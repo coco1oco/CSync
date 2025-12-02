5047592b0ee3052dcfaffd3f195094fd274d7082
Kurtcvsu <kurt.cadorna@cvsu.edu.ph>
2025-12-02 12:33:52 +0800

    merge log in

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import AppRouter from "./Routes/AppRouter"
+import InstallPWA from "./components/installPWA";
 
 export default function App() {
   return (
     <>
+      <InstallPWA />
       <AppRouter />
     </>
   );
